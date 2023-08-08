39b30d7f7f19e5a87a32035fb1d561e32c358400
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-08 15:59:25 +0300

    Task 3 auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Auth class """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """A class to manage the API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns a boolean"""
+        return False
+
+    
+    def authorization_header(self, request=None) -> str:
+        """Returns a str"""
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns a user"""
+        return None
