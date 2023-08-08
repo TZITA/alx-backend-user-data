e99609efdc6c0d6b0b790f9229e21fc40a43b185
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-08 18:30:52 +0300

    task 4 require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,11 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns a boolean"""
+        if path is not None and path[-1] != '/':
+            path += '/'
+        if path is None or excluded_paths is None or\
+                len(excluded_paths) == 0 or path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
