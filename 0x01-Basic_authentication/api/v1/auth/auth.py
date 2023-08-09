4fcfeba7353e8578dd5fd27689fe4ec86b41bc6f
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-09 16:57:55 +0300

    last task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,14 @@ class Auth():
         if path is not None and path[-1] != '/':
             path += '/'
         if path is None or excluded_paths is None or\
-                len(excluded_paths) == 0 or path not in excluded_paths:
+                len(excluded_paths) == 0:
+            return True
+        for p in excluded_paths:
+            if p.startswith(path):
+                return False
+            if p[-2] == "*":
+                if path.startswith(p[:-2]):
+                    return False
             return True
         return False
 
