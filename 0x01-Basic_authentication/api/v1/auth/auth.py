f7b55a5f7659f3b627c47de7070da00a2e659e62
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-09 17:14:15 +0300

    last task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,16 +9,14 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns a boolean"""
-        if path is not None and path[-1] != '/':
-            path += '/'
         if path is None or excluded_paths is None or\
-                len(excluded_paths) == 0:
+           len(excluded_paths) == 0:
             return True
         for p in excluded_paths:
-            if path == p:
+            if path.startswith(p) or p.startswith(path):
                 return False
-            if p[-2] == "*":
-                if path.startswith(p[:-2]):
+            if p[-1] == "*":
+                if path.startswith(p[:-1]):
                     return False
         return True
 
