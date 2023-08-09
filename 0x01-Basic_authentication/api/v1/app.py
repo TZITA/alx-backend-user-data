3b4fc820bc0f45e803441ad4a76a4e772c2f02cc
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-09 08:52:35 +0300

    pep8 validated

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,16 +25,15 @@ def bef_req():
     """
     if auth is None:
         return
-    
-    if not auth.require_auth(request.path,
-                            ['/api/v1/status/',
-                                    '/api/v1/unauthorized/',
-                                    '/api/v1/forbidden/']):
+
+    exc_paths = ['/api/v1/status/',
+                 '/api/v1/unauthorized/',
+                 '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, exc_paths):
         return
-    
     if auth.authorization_header(request) is None:
         abort(401)
-    
     if auth.current_user(request) is None:
         abort(403)
 
