b463aa482f6860f61c5ee0b7fa13b02820f7787b
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-10 17:10:04 +0300

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 """Session Authentication"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session Auth class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = uuid.uuid4()
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+
+        return session_id
